97a908d93139dcc20c28f573e46272d129b3741e
Harikrrish-2002 <harikrishnand5840@gmail.com>
2024-10-22 16:27:10 +0530

    Update Matrixmulti.c

--- Matrixmulti.c
+++ Matrixmulti.c
@@ -1,40 +1,54 @@
 #include<stdio.h>
-void main(){
+void main()
+{
     int a[10][10],b[10][10],res[10][10];
     int m,n,p,q,i,j,k;
     printf("Enter row and column of first matrix: ");
     scanf("%d%d",&m,&n);
     printf("Enter first matrix: \n");
-    for(i=0;i<m;i++){
-        for(j=0;j<n;j++){
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
             scanf("%d",&a[i][j]);
         }
     }
     printf("Enter row and column of second matrix: ");
     scanf("%d%d",&p,&q);
     printf("Enter second matrix: \n");
-    for(i=0;i<p;i++){
-        for(j=0;j<q;j++){
+    for(i=0;i<p;i++)
+    {
+        for(j=0;j<q;j++)
+        {
             scanf("%d",&b[i][j]);
         }
     }
-    if(n==p){
-        for(i=0;i<m;i++){
-            for(j=0;j<q;j++){
+    if(n==p)
+    {
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<q;j++)
+            {
                 res[i][j]=0;
-                for(k=0;k<n;k++){
+                for(k=0;k<n;k++)
+                {
                     res[i][j]+=a[i][k]*b[k][j];
                 }
             }
         }
         printf("Result: \n");
-        for(i=0;i<m;i++){
-            for(j=0;j<q;j++){
+        for(i=0;i<m;i++)
+        {
+            for(j=0;j<q;j++)
+            {
+                
                 printf("%d ",res[i][j]);
             }
             printf("\n");
         }
-    }else{
+    }
+    else
+    {
         printf("Matrix multiplication not pssible");
     }
 }
