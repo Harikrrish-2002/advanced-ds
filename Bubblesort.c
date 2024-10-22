245f0ecf842d25638bf2777f6437f64b7168e745
Harikrrish-2002 <harikrishnand5840@gmail.com>
2024-10-22 16:06:19 +0530

    Update Bubblesort.c

--- Bubblesort.c
+++ Bubblesort.c
@@ -3,33 +3,40 @@ void bubbleSort(int a[],int n){
 	int temp,swapped,i,j;
 	for(i=0;i<n;i++){
 		swapped=0;
-		for(j=0;j<n-i-1;j++){
-			if(a[j] > a[j+1]){
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j] > a[j+1])
+			{
 				temp=a[j];
 				a[j]=a[j+1];
 				a[j+1]=temp;
 				swapped=1;
 			}
 		}
-		if(!swapped){
+		if(!swapped)
+		{
 			break;
 		}
 	}
 }
-void printArray(int a[],int n){
+void printArray(int a[],int n)
+{
 	int i;
-	for(i=0;i<n;i++){
+	for(i=0;i<n;i++)
+	{
 		printf("%d ",a[i]);
 	}
 	printf("\n");
 }
-int main(){
+int main()
+{
 	int a[10],n,i;
 	printf("Program for bubble sort\n");
 	printf("Enter the size of array: ");
 	scanf("%d",&n);
 	printf("Enter the array elements : ");
-	for(i=0;i<n;i++){
+	for(i=0;i<n;i++)
+	{
 		scanf("%d",&a[i]);
 	}
 	bubbleSort(a,n);
